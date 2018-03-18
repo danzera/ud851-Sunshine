e85424540f2cbbd537a2dd36a6ee1627d1578184
Dan Zera <dczera@gmail.com>
2018-03-18 14:26:46 -0500

    complete 1-2 exercises

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,29 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.android.sunshine.data.DefaultWeather;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    // DONE (1) Create a field to store the weather display TextView
+    TextView weatherDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        // DONE (2) Use findViewById to get a reference to the weather display TextView
+        weatherDisplay = (TextView) findViewById(R.id.tv_weather_data);
 
-        // TODO (3) Create an array of Strings that contain fake weather data
+        // DONE (3) Create an array of Strings that contain fake weather data
+        String[] weatherData = DefaultWeather.getWeather();
 
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        // DONE (4) Append each String from the fake weather data array to the TextView
+        for (String weatherPattern : weatherData) {
+            weatherDisplay.append(weatherPattern + "\n\n\n");
+        }
     }
 }
---
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/data/DefaultWeather.java
@@ -0,0 +1,27 @@
+/**
+ * Created by danzera on 3/18/18.
+ */
+
+package com.example.android.sunshine.data;
+
+public final class DefaultWeather {
+
+    /**
+     * @return A list of weather patterns
+     */
+    public static String[] getWeather() {
+        return new String[]{
+                "Raining Cats & Dogs",
+                "Cloudy with a Chance of Meatballs",
+                "Torrentially Downpouring",
+                "Blizzard Conditions",
+                "Tsnownami",
+                "Whiteout",
+                "Cold AF",
+                "Wintry Mix",
+                "Warrrrrm",
+                "Rather Blustery",
+                "It's not the heat, it's the humidity"
+        };
+    }
+}
